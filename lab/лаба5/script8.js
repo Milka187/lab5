7971d23e8ee5ea9c7e5af1ea4dcfb4d9cf8d02dc
Milka187 <150803766+Milka187@users.noreply.github.com>
2024-05-23 11:53:16 +0300

    Add files via upload

---
+++ lab/лаба5/script8.js
@@ -0,0 +1,22 @@
+"use strict"; // До сих пор строгий...
+let a = 1;
+let b = 2;
+let c = 3; // Первый способ
+
+let d = 4, e = 5, f = 6; // Второй способ
+
+let g, h, i; // 3 способ обьявления переменных
+// Присваиваем переменным значения:
+g = 7;
+h = 8;
+i = 9;
+
+alert(a); 
+alert(b); 
+alert(c); 
+alert(d); 
+alert(e); 
+alert(f); 
+alert(g); 
+alert(h); 
+alert(i); 
